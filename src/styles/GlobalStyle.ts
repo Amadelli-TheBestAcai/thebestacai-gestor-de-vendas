900be4d67585880543dacc18eb25ec4bdef9051e
MaysharaF <mayshara@alunos.utfpr.edu.br>
2021-12-15 16:40:10 -0300

    Alterando estilização global

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,6 +1,19 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  :root {
+   --white: #FFFFFF;
+   --white-25: #F3F3F3;
+   --white-30: #f7f7f7;
+   --black-opaco: #1C1E23;
+   --grey-70: #bebebe;
+   --grey-80: #acacac;
+
+
+   --orange-200: #FFB956;
+   --orange-250: #FF9D0A;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -8,19 +21,43 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Roboto', sans-serif;
   }
 
-  body {
-    font-size: 16px;
-    background: white;
+  html, body, #root {
+    max-height: 100vh;
+    max-width: 100vw;
 
-  }
+    width: 100%;
+    height: 100%;
+    font: 500 1rem "Roboto", sans-serif;
+    -webkit-text-size-adjust: none;
+    -webkit-font-smoothing: antialiased;
+
+
+    .ant-input, .ant-input-password, .ant-select-selector, .ant-picker {
+      :focus,
+      :active,
+      :hover {
+        outline: none;
+        box-shadow: 0 0 0 0;
+      }
+    }
   }
 
-  :root {
-    --primary-orange: #FF9D0A;
-    --button-add: #007C05;
-    /* --hoverButton-add:  */
-    --button-remove: #FF0000;
-    --hover-bottomLogin: #000000;
-    --mainBackground:  #f4f4f4;
+  *, button, input{
+    border: 0;
+    background: none;
+    outline: none;
   }
-`
+
+  html {
+    color: black;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
--- src/styles/Icons.ts
+++ src/styles/Icons.ts
@@ -1,39 +1,53 @@
-export { PointOfSale } from '@styled-icons/material/PointOfSale'
-export { Motorcycle, CashRegister, Trash } from '@styled-icons/fa-solid'
+//Atuais
+export {
+  Home,
+  ListAlt,
+  CashRegister,
+  Motorcycle,
+  Retweet,
+  Box,
+  Coins,
+  Scroll,
+} from "@styled-icons/fa-solid";
+
+//---------------------------------------------------//
+
+export { PointOfSale } from "@styled-icons/material/PointOfSale";
+export { Trash } from "@styled-icons/fa-solid";
 export {
   ArrowLeftRight,
   CreditCard2Back,
   CreditCard,
   CheckCircleFill,
   TelephoneFill,
-} from '@styled-icons/bootstrap'
-export { BarChart } from '@styled-icons/ionicons-sharp/BarChart'
-export { Cashapp, Ifood, Ubereats } from '@styled-icons/simple-icons'
-export { RemoveCircle, AddCircle } from '@styled-icons/material'
-export { Offer, AddToQueue, User } from '@styled-icons/boxicons-solid'
+} from "@styled-icons/bootstrap";
+export { BarChart } from "@styled-icons/ionicons-sharp/BarChart";
+export { Cashapp, Ifood, Ubereats } from "@styled-icons/simple-icons";
+export { RemoveCircle, AddCircle } from "@styled-icons/material";
+export { Offer, AddToQueue, User } from "@styled-icons/boxicons-solid";
 export {
   ArrowFromLeft,
   ArrowFromRight,
   HomeAlt,
   LogOutCircle,
   ListOl,
-} from '@styled-icons/boxicons-regular'
-export { CloseOutline, SettingsOutline } from '@styled-icons/evaicons-outline'
-export { Repeat } from '@styled-icons/evaicons-solid/Repeat'
-export { Receipt } from '@styled-icons/ionicons-solid/Receipt'
-export { StatusOffline } from '@styled-icons/heroicons-solid/StatusOffline'
-export { AttachMoney } from '@styled-icons/material/AttachMoney'
-export { Ticket } from '@styled-icons/entypo/Ticket'
-export { UserCircle } from '@styled-icons/fa-solid/UserCircle'
-export { WhatsappSquare } from '@styled-icons/boxicons-logos'
-export { EditAlt } from '@styled-icons/boxicons-regular/EditAlt'
-export { History } from '@styled-icons/boxicons-regular/History'
-export { BoxOpen } from '@styled-icons/fa-solid/BoxOpen'
-export { ArrowRight } from '@styled-icons/fa-solid/ArrowRight'
-export { SelectArrows } from '@styled-icons/entypo/SelectArrows'
-export { Printer } from '@styled-icons/boxicons-regular/Printer'
-export { RemoveCircleOutline } from '@styled-icons/material/RemoveCircleOutline'
-export { ClipboardNotes } from '@styled-icons/foundation/ClipboardNotes'
-export { InfoCircle } from '@styled-icons/boxicons-regular/InfoCircle'
-export { FileInvoice } from '@styled-icons/fa-solid/FileInvoice'
-export { Chat } from '@styled-icons/bootstrap/Chat'
+} from "@styled-icons/boxicons-regular";
+export { CloseOutline, SettingsOutline } from "@styled-icons/evaicons-outline";
+export { Repeat } from "@styled-icons/evaicons-solid/Repeat";
+export { Receipt } from "@styled-icons/ionicons-solid/Receipt";
+export { StatusOffline } from "@styled-icons/heroicons-solid/StatusOffline";
+export { AttachMoney } from "@styled-icons/material/AttachMoney";
+export { Ticket } from "@styled-icons/entypo/Ticket";
+export { UserCircle } from "@styled-icons/fa-solid/UserCircle";
+export { WhatsappSquare } from "@styled-icons/boxicons-logos";
+export { EditAlt } from "@styled-icons/boxicons-regular/EditAlt";
+export { History } from "@styled-icons/boxicons-regular/History";
+export { BoxOpen } from "@styled-icons/fa-solid/BoxOpen";
+export { ArrowRight } from "@styled-icons/fa-solid/ArrowRight";
+export { SelectArrows } from "@styled-icons/entypo/SelectArrows";
+export { Printer } from "@styled-icons/boxicons-regular/Printer";
+export { RemoveCircleOutline } from "@styled-icons/material/RemoveCircleOutline";
+export { ClipboardNotes } from "@styled-icons/foundation/ClipboardNotes";
+export { InfoCircle } from "@styled-icons/boxicons-regular/InfoCircle";
+export { FileInvoice } from "@styled-icons/fa-solid/FileInvoice";
+export { Chat } from "@styled-icons/bootstrap/Chat";
