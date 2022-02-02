2a633f290433e8dd132a925aff55b42271f95c78
MaysharaF <mayshara@alunos.utfpr.edu.br>
2022-02-01 22:32:16 -0300

    Corrigindo informações do balanço

--- src/pages/Balance/index.tsx
+++ src/pages/Balance/index.tsx
@@ -4,7 +4,7 @@ import { PieChart } from "react-minimal-pie-chart";
 import DisconectedForm from "../../containers/DisconectedForm";
 
 import Spinner from "../../components/Spinner";
-import pixImg from "../../assets/svg/pixIcon.svg";
+// import pixImg from "../../assets/svg/pixIcon.svg";
 
 import { currencyFormater } from "../../helpers/currencyFormater";
 import { Balance as BalanceModel } from "../../models/balance";
@@ -31,12 +31,14 @@ import {
   OnlineIcon,
   PixIcon,
   LegendDescription,
+  TicketIcon,
 } from "./styles";
 
 const Balance: React.FC = () => {
   const [isConected, setIsConected] = useState(false);
   const [isLoading, setLoading] = useState(true);
   const [balance, setBalance] = useState<BalanceModel>();
+  const [currentTab, setCurrentTab] = useState("delivery");
 
   useEffect(() => {
     async function init() {
@@ -57,10 +59,7 @@ const Balance: React.FC = () => {
           <TabPaneContainer tab_id={1}>
             <LabelCardTab>
               <p>Delivery</p>
-              <span>
-                R$ {currencyFormater(+payload.delivery.total)}
-                {/* R$ {balance?.delivery.total.toFixed(2).replace(".", ",")} */}
-              </span>
+              <span>R$ {currencyFormater(+payload.delivery.total)}</span>
             </LabelCardTab>
           </TabPaneContainer>
         ),
@@ -114,57 +113,110 @@ const Balance: React.FC = () => {
         value: getPercent(payload[tab].debit, payload[tab].total),
         color: "var(--blue-500)",
       },
-
       {
         id: "Pix",
         value: getPercent(payload[tab].pix, payload[tab].total),
         color: "var(--orange-700)",
       },
       {
-        id: "Online",
+        id: tab === "store" ? "Ticket" : "Online",
         value: getPercent(
           tab === "store" ? payload[tab].ticket : payload[tab].online,
           payload[tab].total
         ),
-        color: "var(--orange-400)",
+        color: tab === "store" ? "var(--purple-450)" : "var(--orange-400)",
       },
     ];
   };
 
   const createAmountTypePayments = (tab: string, payload: BalanceModel) => {
-    return [
-      {
-        id: 1,
-        icon: <MoneyIcon />,
-        type: "DINHEIRO",
-        value: payload[tab].money,
-      },
-      {
-        id: 2,
-        icon: <CreditIcon />,
-        type: "CRÉDITO",
-        value: payload[tab].credit,
-      },
-      {
-        id: 3,
-        icon: <DebitIcon />,
-        type: "DÉBITO",
-        value: payload[tab].debit,
-      },
-      {
-        id: 4,
-        icon: <PixIcon src={pixImg} />,
-        type: "PIX",
-        value: payload[tab].pix,
-      },
-      {
+    const response = [];
+
+    if (tab === "store" || tab === "delivery") {
+      response.push(
+        {
+          id: 1,
+          icon: <MoneyIcon />,
+          type: "DINHEIRO",
+          value: payload[tab].money,
+        },
+        {
+          id: 2,
+          icon: <CreditIcon />,
+          type: "CRÉDITO",
+          value: payload[tab].credit,
+        },
+        {
+          id: 3,
+          icon: <DebitIcon />,
+          type: "DÉBITO",
+          value: payload[tab].debit,
+        },
+        {
+          id: 4,
+          // icon: <PixIcon src={pixImg} />,
+          type: "PIX",
+          value: payload[tab].pix,
+        }
+      );
+    }
+
+    if (tab === "store") {
+      response.push({
         id: 5,
+        icon: <TicketIcon />,
+        type: "TICKET",
+        value: payload[tab].ticket,
+      });
+    }
+
+    if (tab === "delivery") {
+      response.push({
+        id: 6,
         icon: <OnlineIcon />,
         type: "ONLINE",
         value: payload[tab].online,
-      },
-    ];
+      });
+    }
+
+    if (tab === "billing") {
+      response.push(
+        {
+          id: 7,
+          icon: <OnlineIcon />,
+          type: "VENDAS",
+          value: payload[tab].sales,
+        },
+        {
+          id: 8,
+          icon: <OnlineIcon />,
+          type: "VENDAS DELIVERY",
+          value: payload[tab].delivery_sales,
+        },
+        {
+          id: 9,
+          icon: <OnlineIcon />,
+          type: "VENDAS LOJA",
+          value: payload[tab].store_sales,
+        },
+        {
+          id: 10,
+          icon: <OnlineIcon />,
+          type: "TICKET MÉDIO DELIVERY",
+          value: payload[tab].delivery_ticket,
+        },
+        {
+          id: 11,
+          icon: <OnlineIcon />,
+          type: "TICKET MÉDIO LOJA",
+          value: payload[tab].store_ticket,
+        }
+      );
+    }
+
+    return response;
   };
+
   const legendGraph = [
     {
       label: "Dinheiro",
@@ -177,6 +229,10 @@ const Balance: React.FC = () => {
     {
       label: "Débito",
       color: "var(--blue-500)",
+    },
+    {
+      label: "Ticket",
+      color: "var(--purple-450)",
     },
     {
       label: "Pix",
@@ -199,7 +255,10 @@ const Balance: React.FC = () => {
           </Header>
 
           <TabContainer>
-            <Tabs defaultActiveKey="1">
+            <Tabs
+              defaultActiveKey="1"
+              onChange={(id_tab) => setCurrentTab(id_tab)}
+            >
               {createTabPanes(balance).map((_tab) => (
                 <TabPane tab={_tab.label} key={_tab.id}>
                   <Content>
@@ -271,165 +330,6 @@ const Balance: React.FC = () => {
         <DisconectedForm />
       )}
     </Container>
-    /* 
-      {isLoading ? (
-        <Spinner />
-      ) : isConected ? (
-        <CardContainer>
-          <Card>
-            <CardHeader>DELIVERY</CardHeader>
-            <CardBody>
-              <CardRow style={{ background: "#4356FF", width: "100%" }}>
-                <Title>
-                  R$ {balance.delivery.total.toFixed(2).replace(".", ",")}
-                </Title>
-              </CardRow>
-              <CardRow>
-                <MoneyIcon />
-                <CardContent>
-                  R$ {balance.delivery.money.toFixed(2).replace(".", ",")}
-                  <Description>Dinheiro</Description>
-                </CardContent>
-              </CardRow>
-              <CardRow>
-                <CreditIcon />
-                <CardContent>
-                  R$ {balance.delivery.credit.toFixed(2).replace(".", ",")}
-                  <Description>Crédito</Description>
-                </CardContent>
-              </CardRow>
-              <CardRow>
-                <DebitIcon />
-                <CardContent>
-                  R$ {balance.delivery.debit.toFixed(2).replace(".", ",")}
-                  <Description>Débito</Description>
-                </CardContent>
-              </CardRow>
-              <CardRow>
-                <img
-                  src={PixLogo}
-                  style={{
-                    width: "3vw",
-                    height: "5vh",
-                    margin: "0% 7% 0% 13%",
-                  }}
-                />
-                <CardContent>
-                  R$ {balance.delivery.pix.toFixed(2).replace(".", ",")}
-                  <Description>PIX</Description>
-                </CardContent>
-              </CardRow>
-              <CardRow>
-                <CheckOnline />
-                <CardContent>
-                  R$ {balance.delivery.online.toFixed(2).replace(".", ",")}
-                  <Description>Online</Description>
-                </CardContent>
-              </CardRow>
-            </CardBody>
-            <CardFooter />
-          </Card>
-          <Card>
-            <CardHeader style={{ background: "#007305" }}>LOJA</CardHeader>
-            <CardBody green>
-              <CardRow style={{ background: "#058A0A", width: "100%" }}>
-                <Title>
-                  R$ {balance.store.total.toFixed(2).replace(".", ",")}
-                </Title>
-              </CardRow>
-              <CardRow green>
-                <MoneyIcon />
-                <CardContent>
-                  R$ {balance.store.money.toFixed(2).replace(".", ",")}
-                  <Description>Dinheiro</Description>
-                </CardContent>
-              </CardRow>
-              <CardRow green>
-                <CreditIcon />
-                <CardContent>
-                  R$ {balance.store.credit.toFixed(2).replace(".", ",")}
-                  <Description>Crédito</Description>
-                </CardContent>
-              </CardRow>
-              <CardRow green>
-                <DebitIcon />
-                <CardContent>
-                  R$ {balance.store.debit.toFixed(2).replace(".", ",")}
-                  <Description>Débito</Description>
-                </CardContent>
-              </CardRow>
-              <CardRow green>
-                <img
-                  src={PixLogo}
-                  style={{
-                    width: "3vw",
-                    height: "5vh",
-                    margin: "0% 7% 0% 13%",
-                  }}
-                />
-                <CardContent>
-                  R$ {balance.store.pix.toFixed(2).replace(".", ",")}
-                  <Description>PIX</Description>
-                </CardContent>
-              </CardRow>
-              <CardRow green>
-                <TicketIcon />
-                <CardContent>
-                  R$ {balance.store.ticket.toFixed(2).replace(".", ",")}
-                  <Description>Ticket</Description>
-                </CardContent>
-              </CardRow>
-            </CardBody>
-            <CardFooter green />
-          </Card>
-          <Card>
-            <CardHeader style={{ background: "#FF9D0A" }}>
-              FATURAMENTO
-            </CardHeader>
-            <CardBody black fontWhite>
-              <CardRow style={{ background: "#FF9D0A", width: "100%" }}>
-                <Title>
-                  R$ {balance.billing.total.toFixed(2).replace(".", ",")}
-                </Title>
-              </CardRow>
-              <CardRow black>
-                <CardContent>
-                  <Description white>VENDAS</Description>
-                  {balance.billing.sales}
-                </CardContent>
-              </CardRow>
-              <CardRow black>
-                <CardContent>
-                  <Description white>VENDAS DELIVERY</Description>
-                  {balance.billing.delivery_sales}
-                </CardContent>
-              </CardRow>
-              <CardRow black>
-                <CardContent>
-                  <Description white>VENDAS LOJA</Description>
-                  {balance.billing.store_sales}
-                </CardContent>
-              </CardRow>
-              <CardRow black>
-                <CardContent>
-                  <Description white>TICKET MÉDIO DELIVERY</Description>
-                  R${" "}
-                  {balance.billing.delivery_ticket.toFixed(2).replace(".", ",")}
-                </CardContent>
-              </CardRow>
-              <CardRow black>
-                <CardContent>
-                  <Description white>TICKET MÉDIO LOJA</Description>
-                  R$ {balance.billing.store_ticket.toFixed(2).replace(".", ",")}
-                </CardContent>
-              </CardRow>
-            </CardBody>
-            <CardFooter black />
-          </Card>
-        </CardContainer>
-      ) : (
-        <DisconectedForm />
-      )} */
   );
 };
 
