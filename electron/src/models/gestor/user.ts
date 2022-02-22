4ee14384154ace563e4519eddeb1f09beeadd710
Wellynton Delfino <wnodelfino@gmail.com>
2022-02-22 10:17:56 -0300

    Adicionando models user e product

---
+++ electron/src/models/gestor/user.ts
@@ -0,0 +1,12 @@
+export interface UserDto {
+  id: number;
+  username: string;
+  password: string;
+  name: string;
+  email: string;
+  level: number;
+  image: string;
+  token: string;
+  is_actived: boolean;
+  permissions: string[];
+}
