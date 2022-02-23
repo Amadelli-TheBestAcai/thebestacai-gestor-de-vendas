bac9c5c2470dc83649462281078eb8bcfec819df
Wellynton Delfino <wnodelfino@gmail.com>
2022-02-23 11:11:12 -0300

    Mudadndo método updateStoreCashObservation

--- electron/src/usecases/storeCash/index.ts
+++ electron/src/usecases/storeCash/index.ts
@@ -4,3 +4,4 @@ export * from "./closeStoreCash";
 export * from "./openStoreCash";
 export * from "./getStoreCashHistoryService";
 export * from "./getAvailableStoreCashes";
+export * from "./updateStoreCashObservation";
---
+++ electron/src/usecases/storeCash/updateStoreCashObservation.ts
@@ -0,0 +1,33 @@
+import { IUseCaseFactory } from "../useCaseFactory.interface";
+import odinApi from "../../providers/odinApi";
+import { StoreCashDto } from "../../models/gestor";
+import { getCurrentStoreCash } from "./getCurrentStoreCash";
+import { useCaseFactory } from "../useCaseFactory";
+
+interface Request {
+  observation: string;
+}
+
+class UpdateStoreCashObservation implements IUseCaseFactory {
+  constructor(private getCurrentStoreCashUseCase = getCurrentStoreCash) {}
+
+  async execute({ observation }: Request): Promise<void> {
+    const { response: cashier, has_internal_error: errorOnStoreCash } =
+      await useCaseFactory.execute<StoreCashDto | undefined>(
+        this.getCurrentStoreCashUseCase
+      );
+
+    if (errorOnStoreCash) {
+      throw new Error("Falha ao obter caixa atual");
+    }
+    if (!cashier) {
+      throw new Error("Caixa não encontrado");
+    }
+
+    await odinApi.put(`/cash_history/${cashier?.history_id}`, {
+      observation,
+    });
+  }
+}
+
+export const updateStoreCashObservation = new UpdateStoreCashObservation();
