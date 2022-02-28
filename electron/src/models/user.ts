b050c1ecdc392e211673e22e63871991fcc758c8
Wellynton Delfino <wnodelfino@gmail.com>
2022-02-28 18:31:26 -0300

    Voltando método login em models

--- electron/src/models/user.ts
+++ electron/src/models/user.ts
@@ -23,9 +23,73 @@ class User extends BaseRepository<Entity> {
     super(storageName);
   }
 
-  async get(): Promise<Entity | undefined> {
-    const users = await this.getAll();
-    return users.find((_user) => _user.is_actived);
+  async login(username: string, password: string): Promise<Entity | undefined> {
+    const hasInternet = await checkInternet();
+    if (hasInternet) {
+      const {
+        data: { access_token },
+      } = await janusApi.post("user/login", { username, password });
+      if (!access_token) {
+        return undefined;
+      }
+      const hashedPassword = await criptography.hash(password);
+      const userPayload = {
+        ...jwt_decode<Entity>(access_token),
+        username,
+        password: hashedPassword,
+        token: access_token,
+        is_actived: true,
+      };
+      this.loggedUser = userPayload;
+
+      let users = await this.getAll();
+      users = users.map((_user) => ({
+        ..._user,
+        is_actived: false,
+      }));
+
+      const userIndex = users.findIndex(
+        (_user) => _user.email === userPayload.email
+      );
+
+      if (userIndex >= 0) {
+        users[userIndex] = userPayload;
+        await this.clear();
+        await this.createMany(users);
+      } else {
+        await this.clear();
+        await this.createMany([...users, userPayload]);
+      }
+
+      return userPayload;
+    } else {
+      let users = await this.getAll();
+      users = users.map((_user) => ({
+        ..._user,
+        is_actived: false,
+      }));
+
+      const userIndex = users.findIndex((_user) => _user.username === username);
+
+      if (userIndex < 0) {
+        return undefined;
+      }
+
+      const samePassword = await criptography.compare(
+        password,
+        users[userIndex].password
+      );
+      if (!samePassword) {
+        return undefined;
+      }
+
+      users[userIndex].is_actived = true;
+      this.loggedUser = users[userIndex];
+      await this.clear();
+      await this.createMany(users);
+
+      return users[userIndex];
+    }
   }
 }
 
