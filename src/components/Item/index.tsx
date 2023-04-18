7239decb3bafb500c5bf81e8e6fadeaef1443a9b
Lucas Silva <lucas.augusto7969@gmail.com>
2023-04-18 16:42:53 -0300

    feat: add warning no input de itemsOutCart

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -30,11 +30,20 @@ const Item: React.FC<IProps> = ({ item }) => {
 
   const removeItem = async (): Promise<void> => {
     setModalState(true);
+    let errorMessage = '';
+
     if (reasson.length < 3) {
+      errorMessage = 'Digite um motivo válido para a remoção do item de seu carrinho.';
+    }
+
+    if (reasson.length > 100) {
+      errorMessage = 'O motivo não deve ultrapassar 100 caracteres.';
+    }
+
+    if (errorMessage) {
       notification.warning({
-        message: "Oops! ",
-        description:
-          "Digite um motivo válido para a remoção do item de seu carrinho.",
+        message: 'Oops!',
+        description: errorMessage,
         duration: 5,
       });
       return;
