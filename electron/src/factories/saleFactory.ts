62f5ad10abfd1a59e82f7cf6e4839f372f627c58
anacarlapaiva <acpaiva99@outlook.com>
2023-07-20 11:37:11 -0300

    fix: ajustando a sale factory de acordo com a nova chamada

--- electron/src/factories/saleFactory.ts
+++ electron/src/factories/saleFactory.ts
@@ -100,12 +100,21 @@ export const saleFactory = {
   getCampaignReward: async (cpf: string) =>
     await useCaseFactory.execute<
       {
-        id: number;
-        description: string;
-        value: number;
-        is_taked: boolean;
-        refused: boolean;
-      }[]
+        points_customer: number;
+        campaignReward: {
+          id: number;
+          campaign_id: number;
+          customer_reward_id: number;
+          description: string;
+          url_image: string;
+          s3_key: string;
+          points_reward: number;
+          created_at: string;
+          updated_at: string;
+          deleted_at: string;
+          product_id: number;
+        }[];
+      }
     >(getCampaignReward, {
       cpf,
     }),
