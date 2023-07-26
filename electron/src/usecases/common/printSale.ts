4d3f04148d8939c2a35f8930abf3920aa1481f47
anacarlapaiva <acpaiva99@outlook.com>
2023-07-26 17:57:03 -0300

    fix: voltando o código como era antes

--- electron/src/usecases/common/printSale.ts
+++ electron/src/usecases/common/printSale.ts
@@ -5,7 +5,7 @@ import { StoreDto, SettingsDto } from "../../models/gestor";
 import { SaleFromApiDTO } from "../../models/dtos/salesFromApi";
 import { SaleDto } from "../../models/gestor/sale";
 import { replaceSpecialChars } from "../../helpers/replaceSpecialChars";
-import * as jwt from "jsonwebtoken";
+import * as jwt from 'jsonwebtoken';
 import env from "../../providers/env.json";
 import {
   printer as ThermalPrinter,
@@ -129,8 +129,8 @@ class PrintSale implements IUseCaseFactory {
 
     const totalItems = sale.items
       //@ts-ignore
-     .reduce(
-        (total, item) => total + Number(item.storeProduct?.price_unit) ?? 0 * +item.quantity,
+      .reduce(
+        (total, item) => total + +item.storeProduct.price_unit * +item.quantity,
         0
       )
       .toFixed(2)
@@ -144,11 +144,11 @@ class PrintSale implements IUseCaseFactory {
       { text: totalItems + "R$", align: "CENTER", cols: 10 },
     ]);
     this.printerFormater.drawLine();
-
+    
     if (sale.nfce?.qrcode_url) {
       this.printerFormater.table(["QRCode NOTA FISCAL"]);
       this.printerFormater.alignCenter();
-
+      
       this.printerFormater.printQR(sale.nfce.qrcode_url, {
         correction: "M",
         cellSize: 7,
@@ -156,33 +156,36 @@ class PrintSale implements IUseCaseFactory {
       this.printerFormater.newLine();
     }
 
-    const access_token = jwt.sign(
-      {
-        ref: sale.ref,
-        cpf: null,
-        cash_history_id: sale.cash_history_id,
-        sale_id: sale.id,
-        store_id: store?.company_id,
-        total_sold: totalItems,
-      },
-      env.TOKEN_SECRET_NPS,
-      {
-        expiresIn: "1d",
-      }
-    );
-    console.log(access_token);
+      const access_token = jwt.sign(
+        {
+          ref: sale.ref,
+          cpf: null,
+          cash_history_id: sale.cash_history_id,
+          sale_id: sale.id,
+          store_id: store?.company_id,
+          total_sold: totalItems
+        },
+        env.TOKEN_SECRET_NPS,
+        {
+          expiresIn: '1d',
+        },
+      );
+      console.log(access_token)
 
-    this.printerFormater.table(["QRCode Avaliação NPS"]);
-    this.printerFormater.println(
-      `Utilize este QRCode para ser direcionado para nos avaliar :)`
-    );
-    this.printerFormater.alignCenter();
-    this.printerFormater.printQR(`${env.NPS_URL}/qrcode?hash=${access_token}`, {
-      correction: "M",
-      cellSize: 7,
-    });
-    this.printerFormater.newLine();
-    this.printerFormater.cut();
+      this.printerFormater.table(["QRCode Avaliação NPS"]);
+      this.printerFormater.println(
+        `Utilize este QRCode para ser direcionado para nos avaliar :)`
+      );
+      this.printerFormater.alignCenter();
+      this.printerFormater.printQR(
+        `${env.NPS_URL}/qrcode?hash=${access_token}`,
+        {
+          correction: "M",
+          cellSize: 7,
+        }
+      );
+      this.printerFormater.newLine();
+      this.printerFormater.cut();
 
     Printer.printDirect({
       data: this.printerFormater.getBuffer(),
