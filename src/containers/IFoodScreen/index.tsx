91e4e2a85f4d0403e7834147d747254d3d43609e
anacarlapaiva <acpaiva99@outlook.com>
2023-08-07 16:47:21 -0300

    feat: criação da página do ifood

---
+++ src/containers/IFoodScreen/index.tsx
@@ -0,0 +1,311 @@
+import React, { useState } from 'react';
+
+import CardComponent from '../../components/OrderCardComponent';
+import OrderPageIfood from '../../containers/OrderPageIfood';
+import AuthIfood from '../AuthIfood';
+import { useUser } from "../../hooks/useUser";
+const { Option } = Select;
+import {
+  Container,
+  Select,
+  SideMenu,
+  SearchIcon,
+  ButtonPause,
+  ContentGeneral,
+  Button,
+  ContentButton,
+  Tabs,
+  InputWithSearchIcon,
+  FilterCheckbox,
+  PageContent,
+  Input,
+  CardScheduled,
+  ContentInsideMenu,
+  TabPaneElement,
+  ContentHome,
+  CardHome,
+  ContentSideMenu,
+  Footer,
+  Collapse,
+  CollapseHeader,
+  PanelAnt,
+  PanelContent,
+  ItemInfo,
+  ItemTitle,
+  ItemDescription,
+  ItemPrice,
+  ContentPrice,
+  PlayIcon,
+  PauseIcon,
+  Dropdown,
+  ContentSelect,
+  ContentMenuItems
+} from './styles';
+const TesteModule = [
+  {
+    id: 1,
+    title: ['Categoria item normal',],
+    item: 'Sanduíche',
+    description: 'lorem ipsum',
+    price: 'R$10,00'
+  },
+  {
+    id: 2,
+    title: 'Categoria 2',
+    item: 'Açaí',
+    description: 'hasiuhcfiah',
+    price: 'R$10,00'
+  },
+  {
+    id: 3,
+    title: 'Categoria 333',
+    item: 'Pão com mandioca',
+    description: 'lorem ipsum',
+    price: 'R$10,00'
+  },
+]
+
+const IFoodScreen: React.FC = () => {
+  const [activeTab, setActiveTab] = useState('pedidos');
+  const [selectedOption, setSelectedOption] = useState<string>('agora');
+  const [totalChecked, setTotalChecked] = useState(0);
+  const [auth, setAuth] = useState<any>();
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [filters, setFilters] = useState({
+    statusChecked: false,
+    channelChecked: false,
+    othersChecked: false,
+  });
+  const { user } = useUser();
+
+  const handleCheckboxChange = (checkboxName, isChecked) => {
+    setFilters({ ...filters, [checkboxName]: isChecked });
+
+    const checkedCount = Object.values({ ...filters, [checkboxName]: isChecked }).filter(Boolean).length;
+    setTotalChecked(checkedCount);
+  };
+
+  const menuCheckbox = (
+    <div>
+
+      <FilterCheckbox
+        checked={filters.statusChecked}
+        onChange={(e) => handleCheckboxChange("statusChecked", e.target.checked)}
+      >
+        Teste
+      </FilterCheckbox>
+
+      <FilterCheckbox
+        checked={filters.channelChecked}
+        onChange={(e) => handleCheckboxChange("channelChecked", e.target.checked)}
+      >
+        Teste2
+      </FilterCheckbox>
+
+      <FilterCheckbox
+        checked={filters.othersChecked}
+        onChange={(e) => handleCheckboxChange("othersChecked", e.target.checked)}
+      >
+        Teste3
+      </FilterCheckbox>
+    </div>
+  );
+
+  const screenTypes = [
+    {
+      id: 1,
+      value: "agora",
+    },
+    {
+      id: 2,
+      value: "agendado",
+    },
+    {
+      id: 3,
+      value: "card",
+    },
+  ];
+
+  return (
+    <>
+      {!auth ? (<AuthIfood />
+      ) : (
+        <Container>
+          <Tabs activeKey={activeTab} defaultActiveKey="pedidos" onChange={(key) => setActiveTab(key)}>
+            <TabPaneElement tab="Pedidos" key="pedidos" />
+            <TabPaneElement tab="Cardápio" key="cardapio" />
+          </Tabs>
+          {activeTab === 'pedidos' ? (
+            <ContentGeneral>
+              <SideMenu>
+                <>
+                  <ContentSelect>
+                    <Select
+                      value={selectedOption}
+                      onChange={(value) => setSelectedOption(String(value))}
+                    >
+                      {screenTypes.map((item) => (
+                        <Option key={item.id} value={item.value}>
+                          {item.value}
+                        </Option>
+                      ))}
+                    </Select>
+                  </ContentSelect>
+
+                  {selectedOption === "agora" ? (
+                    <ContentSideMenu>
+                      <ContentButton>
+                        <InputWithSearchIcon placeholder="Buscar pedido" prefix={<SearchIcon />} />
+                        <Dropdown
+                          overlay={menuCheckbox}
+                          placement="bottomRight"
+                          trigger={["click"]}
+                          visible={dropdownVisible}
+                          onVisibleChange={(visible) => setDropdownVisible(visible)}
+                        >
+                          <Button icon={<SearchIcon />}>
+                            {totalChecked > 0 && `${totalChecked}`} Aplicar filtro
+                          </Button>
+
+                        </Dropdown>
+                      </ContentButton>
+
+                      <CardComponent
+                        delivery='teste'
+                        order='333'
+                        status='teste'
+                        onClick={() => {}}
+                      />
+ 
+                      <Footer>
+                        <div className="content-footer">
+                          <div className="items">
+                            <span className="order-name">Pedidos (0): </span>
+                            <span>R$ 0,00</span>
+                          </div>
+                          <div className="items">
+                            <span className="order-name">Online (0): </span>
+                            <span>R$ 0,00</span>
+                          </div>
+                        </div>
+                      </Footer>
+                    </ContentSideMenu>
+                  ) : (
+                    <ContentInsideMenu>
+                      <p>aqui vai ficar um card</p>
+                      <ButtonPause>Pausar agendamento</ButtonPause>
+                    </ContentInsideMenu>
+                  )}
+
+                </>
+              </SideMenu>
+              <PageContent>
+                {selectedOption === "agora" ? (
+                  <Container>
+                    <h1>👋 Olá, {user.name}</h1>
+                    <ContentHome>
+                      <CardHome>
+                        <h3>Horário de funcionamento:</h3>
+
+                        <div className="container-card">
+                          <div className="content">
+                            <span>Hoje, 02/08</span>
+                            <span className="hourTime">00:00 - 23:59</span>
+                          </div>
+                          <div className="content">
+                            <span>Hoje, 02/08</span>
+                            <span className="hourTime">00:00 - 23:59</span>
+                          </div>
+                        </div>
+                      </CardHome>
+                      <CardHome>
+                        <h3>Itens pausados no cardápio:</h3>
+                        <p>0</p>
+                      </CardHome>
+                    </ContentHome>
+                  </Container>
+                ) : selectedOption === "agendado" ? (
+                  <ContentGeneral>
+                    <CardScheduled>
+                      <h2>Pedidos agendados</h2>
+
+                      <p>Aqui você encontra os <b>pedidos agendados</b> feitos na sua loja</p>
+                      <p>O agendamento ajuda a prever a demanda que virá para a sua cozinha e seus entregadores</p>
+                      <p>Os pedidos agendados irão para a aba <b>"Agora"</b> quando estiver faltando o tempo de entrega configurado no seu restaurante.
+                        Você poderá confirmar ou cancelar o pedido clicando em "Rejeitar pedido".
+                      </p>
+                    </CardScheduled>
+                  </ContentGeneral>
+                ) : selectedOption === "card" && (
+                  <OrderPageIfood />
+                )}
+              </PageContent>
+            </ContentGeneral>
+          ) : (
+            <Container>
+              <ContentMenuItems>
+                <h1>Cardápio</h1>
+
+                <p><b>Pause</b> ou <b>ative rapidamente os itens</b>{" "}
+                  do cardápio da sua loja por aqui. Para fazer edições como incluir, excluir itens ou fotos, <br />
+                  acesse o {" "}
+                  <a href="https://portal.ifood.com.br/menu"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Portal do Parceiro.
+                  </a>
+                </p>
+                <p>As alterações dos cardápios podem demorar até 5 minutos para aparecerem aos clientes</p>
+
+                <Input prefix={<SearchIcon />} placeholder='Buscar item do cardápio' />
+
+                <Collapse expandIconPosition="right">
+                  <CollapseHeader>
+                    <span>Categoria item</span>
+                    <button onClick={() => setIsPlaying((prevState) => !prevState)}>
+                      {isPlaying ? <PauseIcon /> : <PlayIcon />}
+                    </button>
+                  </CollapseHeader>
+
+                  {TesteModule.map((_module) => (
+                    <PanelAnt
+                      header={
+                        <div>
+                          {_module.item}
+                          <ItemDescription style={{ marginLeft: '1rem' }}>{_module.description}</ItemDescription>
+                        </div>
+                      }
+                      key={_module.id}
+                    >
+                      <PanelContent>
+                        <ItemInfo>
+                          <ItemTitle>{_module.title}</ItemTitle>
+
+                          <ContentPrice>
+                            <ItemDescription>{_module.description}</ItemDescription>
+
+                            <div>
+                              <ItemPrice>{_module.price}</ItemPrice>
+                              <button onClick={() => setIsPlaying((prevState) => !prevState)}>
+                                {isPlaying ? <PauseIcon size={5} /> : <PlayIcon size={5} />}
+                              </button>
+                            </div>
+                          </ContentPrice>
+                        </ItemInfo>
+                      </PanelContent>
+                    </PanelAnt>
+                  ))}
+                </Collapse>
+              </ContentMenuItems>
+            </Container>
+          )}
+
+        </Container>
+      )}
+    </>
+  );
+
+};
+
+export default IFoodScreen;
---
+++ src/containers/IFoodScreen/styles.ts
@@ -0,0 +1,505 @@
+import styled from "styled-components";
+import { Search, PlayCircle, PauseCircle } from "../../styles/Icons";
+import {
+  Select as SelectAnt,
+  Button as ButtonAnt,
+  Tabs as TabsAnt,
+  Input as InputAnt,
+  Checkbox as CheckboxAnt,
+  Collapse as CollapseAnt,
+  Dropdown as DropdownAnt,
+} from "antd";
+const { Panel } = CollapseAnt;
+
+export const Option = styled(SelectAnt.Option)``;
+
+export const Dropdown = styled(DropdownAnt)`
+  .ant-dropdown::before{
+    background: var(--gray-10);
+    padding:.8rem;
+  } 
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  p{
+    color: var(--grey-100);
+  }
+`;
+
+export const TabPaneElement = styled(TabsAnt.TabPane)`
+  font-size: 2rem;
+`;
+
+export const SideMenu = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  align-items: flex-start;
+  min-width: 23rem;
+  height: 79vh;
+  box-shadow: 4px 0px 4px 0px rgba(0, 0, 0, 0.25);
+`;
+
+export const MenuContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ContentMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
+
+export const ContentSelect = styled.div`
+  padding: 0 0 0 16px; 
+  width: 100%;
+`;
+
+export const Select = styled(SelectAnt)`
+  border: 0;
+  border-bottom: 1px solid var(--grey-60);
+  width: 95%;
+  height: 40px;
+  padding: 8px;
+  color: var(--grey-60);
+  margin-top: 1rem;
+
+  .ant-select-selector {
+    background: transparent !important;
+    display: flex;
+    align-items: end;
+    padding: 0;
+  }
+
+  .ant-select-arrow {
+    color: var(--grey-60);
+    display: flex;
+    align-items: end;
+  }
+`;
+
+export const Button = styled(ButtonAnt)`
+  display: flex;
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  border: 1px solid var(--grey-60);
+  border-radius: 4px;
+  width: 180px;
+  transition: 0.3s ease-in-out;
+  color: var(--grey-60);
+
+  :hover {
+    border: 2px solid var(--grey-60);
+    color: var(--grey-60);
+  }
+`;
+
+export const InputWithSearchIcon = styled(InputAnt)`
+  display: flex;
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px !important;
+  border: 1px solid var(--grey-60) !important;
+  border-radius: 4px !important;
+  width: 180px;
+  background: transparent !important;
+  margin-right: 10px;
+
+  .ant-input {
+    background: transparent !important;
+    padding: 0 !important;
+    ::placeholder {
+      color: var(--grey-60);
+    }
+  }
+
+  && {
+    .ant-input-prefix {
+      display: flex;
+      align-items: center;
+      color: var(--grey-60);
+    }
+
+    .ant-input-affix-wrapper {
+      ${Button}
+      background: transparent !important;
+    }
+  }
+`;
+
+export const ContentButton = styled.div`
+  display: flex;
+  margin-top: 18px;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 16px;
+`;
+
+export const SearchIcon = styled(Search)`
+  width: 1rem;
+  height: 1rem;
+  margin-right: 0.5rem;
+`;
+
+export const Tabs = styled(TabsAnt)`
+  display: flex;
+  justify-content: start !important;
+  padding: 0 1rem;
+  .ant-tabs-tab {
+    display: flex;
+    font-size: 1.2rem;
+    color: black !important;
+    justify-content: start !important;
+    margin-right: 2rem !important;
+  }
+
+  .ant-tabs-tab-btn:hover{
+    opacity: 50% !important;
+  }
+  .ant-tabs-tab-btn:focus{
+    color: var(--orange-250) !important;
+  }
+
+  .ant-tabs-tab-btn.ant-tabs-tab-active {
+    color: var(--orange-250) !important; 
+  }
+
+  .ant-tabs-tab-btn:active{
+    color: var(--orange-250) !important;
+  }
+
+  .ant-tabs-nav-list {
+    display: flex;
+    justify-content: start;
+  }
+`;
+
+export const FilterButton = styled.div`
+  display: inline-block;
+  position: relative;
+`;
+
+export const FilterDropdown = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  background-color: white;
+  border: 1px solid #d9d9d9;
+  padding: 8px;
+  z-index: 999;
+  min-width: 180px;
+`;
+
+export const FilterButtonContent = styled(Button)`
+  ${Button}
+`;
+
+export const FilterCheckbox = styled(CheckboxAnt)`
+  display: block;
+  margin-bottom: 8px;
+`;
+
+export const ContentGeneral = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+export const ContentMenuItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 1rem;
+`;
+
+export const PageContent = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 1rem 2rem;
+  overflow: hidden;
+`;
+
+export const Card = styled.div`
+    width: 250px;
+    height: 67px;
+    color: red;
+    margin-left: 20px;
+`;
+
+export const Input = styled(InputAnt)`
+   padding: .5rem;
+   width: 50%;
+`;
+
+export const ButtonPause = styled(ButtonAnt)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: transparent;
+  border: 1px solid var(--orange-250);
+  color: var(--orange-250);
+  padding: 1.2rem 1rem;
+  border-radius: .4rem;
+  width: 100%;
+`;
+
+export const CardScheduled = styled.div`
+  width: 40rem;
+  height: 100%;
+  box-shadow: 4px 4px 4px 4px rgba(0, 0, 0, 0.15);
+  border-radius: .4rem;
+  padding: 2rem;
+
+  p {
+    font-weight: 400;
+    font-size: 1rem;
+    color: var(--grey-90);
+    display: block;
+  }
+
+  b{
+    font-weight: 500;
+    color: var(--grey-100);
+  }
+`;
+
+export const ContentSideMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  justify-content: start;
+`;
+
+export const Footer = styled.footer`
+    position: absolute;
+    width: 100%;
+    border-top: 1px solid var(--grey-60);
+    padding: 1rem;
+    bottom: 0;
+
+    .content-footer{
+      display:flex;
+      justify-content: space-between;
+      width: 80%;
+    }
+
+    .order-name{
+      font-size: 1.1rem;
+      color: var(--gray-100);
+    }
+
+    span{
+      display: block;
+      color: var(--grey-100);
+      font-size: 1rem;
+    }
+`;
+
+export const ContentInsideMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+  margin-bottom: 2rem;
+
+  p {
+    color: white;
+  }
+`;
+
+export const ContentHome = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  grid-template-rows: 1fr;
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
+`;
+
+export const CardHome = styled.div`
+  background-color: var(--gray-10);
+  padding: 1.3rem;
+  border-radius: .9rem;
+  display: flex;
+  flex-direction: column;
+  font-weight: 400;
+  color: var(--grey-100);
+
+  h3{
+    font-size: .9rem;
+  }
+
+  span{
+    font-size: 1rem;
+    margin-bottom: 1.2rem;
+  }
+
+  p{
+    font-size: 2rem;
+    font-weight: bold;
+    color: black;
+    margin-bottom: 0;
+  }
+
+  .content{
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .container-card {
+    display: flex;
+    justify-content: space-between;
+    width: 65%;
+  }
+
+  .hourTime{
+    background-color: white;
+    border-radius: .3rem;
+    padding: .2rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .infoDelivery{
+    display: flex;
+    flex-direction: column;
+
+    span{
+      font-size: .9rem;
+      color: var(--gray-60);
+    }
+  }
+`;
+
+export const Collapse = styled(CollapseAnt)`
+  background-color: transparent;
+  border: none;
+
+  .ant-collapse-header,
+  .ant-collapse-content-box {
+    color: var(--grey-100) !important;
+    font-weight: 400;
+    border-left: 1px solid var(--grey-60);
+    border-right: 1px solid var(--grey-60);
+  }
+
+  .ant-collapse-content {
+    border: 1px solid var(--gray-100);
+    border-width: 1px 0;
+  }
+
+  .ant-collapse-content-box {
+    background: var(--gray-20);
+  }
+
+  &:last-child {
+    border-bottom: 1px solid var(--gray-100);
+    .ant-collapse-content {
+      border-bottom-width: 1px;
+    }
+  }
+`;
+
+export const CollapseItem = styled(CollapseAnt)`
+  background-color: transparent;
+  border: none;
+
+  .ant-collapse-header {
+    font-weight: 400;
+    background: var(--white);
+
+    padding-right: 1.5rem;
+
+    border: 1px solid var(--gray-100);
+    border-radius: 0;
+  }
+`;
+
+export const PanelAnt = styled(Panel)`
+  .ant-collapse-header {
+    font-size: 1.3rem;
+  }
+`;
+
+export const CollapseHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-weight: 500;
+  font-size: 1.2rem;
+  padding: 1.5rem 2rem;
+  color: var(--grey-100);
+  background-color: var(--gray-10);
+  border: 1px solid var(--grey-60);
+  border-radius: .4rem .4rem 0 0;
+  margin-top: 1rem;
+`;
+
+export const PanelContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+export const ItemInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const ItemTitle = styled.span`
+  font-size: 1rem;
+  font-weight: 500;
+  color: var(--grey-100);
+`;
+
+export const ItemDescription = styled.span`
+  font-size: 0.9rem;
+  color: var(--grey-60);
+`;
+
+export const ItemPrice = styled.span`
+  font-size: 1rem;
+  font-weight: 500;
+  color: var(--grey-100);
+`;
+
+export const ContentPrice = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+
+  div{
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const PlayIcon = styled(PlayCircle)`
+  width: 1.5rem;
+  height: 1.5rem;
+  color: var(--grey-100);
+  margin-left: .5rem;
+`;
+
+export const PauseIcon = styled(PauseCircle)`
+  width: 1.5rem;
+  height: 1.5rem;
+  color: var(--grey-100);
+  margin-left: .5rem;
+`;
