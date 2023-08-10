692a27d8b1cac908e506a9f5214d012b6e1e89c9
anacarlapaiva <acpaiva99@outlook.com>
2023-08-10 19:12:35 -0300

    feat: criação da tipagem de status do pedido

---
+++ src/models/dtos/ifood/orderStatus.ts
@@ -0,0 +1,9 @@
+export const orderStatus = {
+    placed: 'Pedidos Pendentes',
+    confirmed: 'Pedidos Confirmados',
+    preparation_started: 'Preparação Iniciada',
+    dispatched: 'Pedidos Enviados',
+    ready_to_pickup: 'Prontos para Retirada',
+    cancelled: 'Pedidos Cancelados',
+    concluded: 'Pedidos Concluídos',
+};
