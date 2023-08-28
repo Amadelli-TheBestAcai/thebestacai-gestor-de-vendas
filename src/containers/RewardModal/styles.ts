ebaa4f08db4945a8f3aeb805826bba041ff75701
anacarlapaiva <acpaiva99@outlook.com>
2023-08-28 16:04:59 -0300

    style: estilização

--- src/containers/RewardModal/styles.ts
+++ src/containers/RewardModal/styles.ts
@@ -1,6 +1,7 @@
 import styled, { css } from "styled-components";
-import { Col as ColAnt, Modal as ModalAnt, Button as AntButton } from "antd";
+import { Col as ColAnt, Modal as ModalAnt, Button, Row as RowAnt, Input as InputAnt } from "antd";
 import { Search, MinusCircle, PlusCircle2 } from "../../styles/Icons";
+import { IMaskInput } from "react-imask";
 
 export const GlobalContainer = styled.div`
   display: flex;
@@ -10,15 +11,20 @@ export const GlobalContainer = styled.div`
 
 export const Container = styled.div`
   flex: 1;
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: repeat(0.5fr, 1.5fr);
-  grid-row-gap: 25px;
-  padding-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Row = styled(RowAnt)`
+  width: 100%;
 `;
 
 export const Modal = styled(ModalAnt)`
   user-select: none;
+
+  .ant-modal-body {
+    padding: 20px;
+  }
   .ant-modal-title {
     font-size: 1.3rem;
   }
@@ -26,48 +32,7 @@ export const Modal = styled(ModalAnt)`
   max-height: 50rem !important;
   overflow-y: auto;
 `;
-
-export const FirstContent = styled.div`
-  display: grid;
-  grid-template-columns: 320px repeat(3, 1fr);
-  grid-template-rows: 1fr;
-  font-size: 1.1rem;
-  font-weight: 500;
-  border: 1px solid var(--gray-50);
-  border-radius: 8px;
-  padding: 0.5rem 1rem;
-  align-items: center;
-
-  .name {
-    font-size: 1.4rem;
-    font-weight: 700;
-  }
-`;
-
-export const Header = styled.header`
-  display: flex;
-  width: 100%;
-  height: 3rem;
-  background: var(--black-opaco);
-  color: white;
-  border-radius: 12px;
-  text-transform: uppercase;
-
-  /*Responsive 1600*/
-  @media (max-width: 1600px) {
-    font-size: 0.9rem;
-  }
-
-  /*Responsive 1440*/
-  @media (max-width: 1440px) {
-    font-size: 0.8rem;
-  }
-
-  /*Responsive 1366*/
-  @media (max-width: 1366px) {
-    font-size: 0.7rem;
-  }
-`;
+;
 
 export const Col = styled(ColAnt)`
   display: flex;
@@ -111,19 +76,29 @@ export const CustomerInfo = styled.div`
   }
 `;
 
-export const InputSearchReward = styled.input`
+export const InputSearchReward = styled(IMaskInput)`
   height: 2.5rem;
-  width: 35%;
   border: 1px solid var(--gray-50);
   padding: 5px;
   border-radius: 5px;
+  width: 100%;
+  text-transform: uppercase;
 `;
+
+export const InputMask = styled(IMaskInput)`
+  width: 100%;
+  border: 1px solid var(--gray-50);
+  height: 2.5rem;
+  border-radius: 5px;
+  padding: 5px;
+`;
+
 
 export const ButtonSearch = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 4rem;
+  width: 100%;
   height: 100%;
   background: var(--orange-250);
   color: white;
@@ -168,25 +143,100 @@ export const RewardRow = styled.div`
   }
 `;
 
-export const RewardContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 1rem;
-
-  overflow-y: auto;
-  overflow-x: hidden;
+export const InfoClient = styled.div`
+  display: block;
+  margin: 0 0 1rem 0;
+  p {
+    font-weight: 700;
+    font-size: 1.2rem;
+    color: var(--gray-250);
+    text-transform: capitalize;
+    margin: 0;
+  }
 `;
 
-export const ContentItemRow = styled.div`
-  max-height: 20rem;
+export const ContentReward = styled.div`
   width: 100%;
-  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
-  @media (max-width: 1600px) {
-    max-height: 15rem;
+interface ICustomerProps {
+  available: boolean;
+}
+
+export const DataClient = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+
+  span {
+    font-weight: 700;
+    font-size: .8rem;
+    margin-bottom: .6rem;
+  }  
+`;
+
+export const ContentGeneral = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  .max-points{
+    font-size: .8rem;
+    color: var(--grey-70);
+    font-weight: 400;
+    justify-content: end;
+    display: flex;
+    margin-top: .5rem;
   }
+`;
+
+export const Value = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const CustomerPoints = styled.div<ICustomerProps>`
+ text-align: center;
+ font-size: 2rem;
+ font-weight: 800;
+ color: ${({ available }) => (available ? 'var(--orange-250)' : 'var(--gray-250)')};
+
+ span{
+  color: var(--grey-100);
+  font-size: 1rem;
+  font-weight: 400;
+  margin-left: .2rem;
+ }
+`;
+
+export const PointsCustomerContainer = styled.div`
+  padding: 1.2rem;
+  background-color: var(--soft-orange);
+`;
+
+export const ProgressBar = styled.div`
+  width: 100%;
+  height: 8px;
+  background-color: var(--gray-25);
+  border-radius: 4px;
+`;
+
+interface IProgressBarProps {
+  actived: string;
+}
+
+export const ProgressBarActived = styled.div<IProgressBarProps>`
+  height: 8px;
+  background-color: var(--orange-250);
+  border-radius: 4px;
+  ${({ actived }) => css`
+    width: ${actived};
+  `}
 `;
 
 export const SearchIcon = styled(Search)`
@@ -199,17 +249,6 @@ export const SearchIcon = styled(Search)`
     width: 0.8rem;
     height: 0.8rem;
   }
-`;
-
-export const CardReward = styled.div<{ invalid: boolean }>`
-  display: flex;
-  align-items: center;
-  width: 100%;
-  border-radius: 12px;
-  border: 1px solid var(--gray-50);
-  margin-bottom: 1rem;
-  cursor: ${(props) => (props.invalid ? "not-allowed" : "pointer")};
-  ${(props) => props.invalid && ` opacity: 30%; `}
 `;
 
 export const ImgContent = styled.img`
@@ -272,6 +311,18 @@ export const ButtonCancel = styled.button`
   margin-right: 3rem;
 `;
 
+export const ImgReward = styled.img`
+  width: 12rem;
+  height: 12rem;
+  object-fit: contain;
+  margin-top: 1rem;
+`;
+
+export const TitleReward = styled.span`
+  font-size: 2rem;
+  font-weight: 500;
+`;
+
 export const ButtonSave = styled.button`
   padding: 10px;
   font-weight: 500;
@@ -302,6 +353,7 @@ export const PlusIconContainer = styled.div<{ disabled: boolean }>`
   opacity: ${(props) => (props.disabled ? 0.6 : 1)};
   display: inline-block;
 `;
+
 export const DecreaseIcon = styled(MinusCircle)`
   ${Icon}
   color: red;
