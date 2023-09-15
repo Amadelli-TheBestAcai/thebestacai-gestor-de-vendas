074b2a04790c70278e14f60b487168eac047f4f1
Wirlley Delfino <wirlley_d@hotmail.com>
2023-09-15 14:37:55 -0300

    ajustando geração de qrcode para nps

--- electron/src/usecases/useCaseFactory.ts
+++ electron/src/usecases/useCaseFactory.ts
@@ -2,31 +2,26 @@ import { BaseRepository } from "../repository/baseRepository";
 import { StorageNames } from "../repository/storageNames";
 import { IUseCaseFactory } from "./useCaseFactory.interface";
 import { ErrorDto } from "../models/dtos/error";
-import { appInsights } from "../providers/appInsights";
 import { UseCaseFactoryResponse } from "./useCaseFactoryResponse.interface";
 
 class UseCaseFactory {
   constructor(
     private errorsRepository = new BaseRepository<ErrorDto>(StorageNames.Errors)
-  ) { }
+  ) {}
 
   async execute<T>(
     useCase: IUseCaseFactory,
     params?: any
   ): Promise<UseCaseFactoryResponse<T>> {
-    // appInsights.start();
     try {
       const response = await useCase.execute(params);
-      // await appInsights.finish(useCase.constructor.name, {
-      //   success: true,
-      //   params,
-      // });
+
       return {
         response,
         has_internal_error: false,
       };
     } catch (error: any) {
-      console.log(error)
+      console.log(error);
       let error_message = error.message;
       if (error?.response) {
         if (error?.response?.data?.message) {
@@ -42,11 +37,6 @@ class UseCaseFactory {
       //   },
       // });
 
-      // await appInsights.finish(useCase.constructor.name, {
-      //   error_message,
-      //   success: false,
-      //   error,
-      // });
       return {
         response: undefined,
         has_internal_error: true,
