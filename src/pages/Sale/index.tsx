35188748a8a20d5da3b54054209d06602baee634
Lucas Silva <lucas.augusto7969@gmail.com>
2023-09-28 16:20:56 -0300

    fix: corrigido bug que dava ao tentar baixar uma nota fiscal em pdf

--- src/pages/Sale/index.tsx
+++ src/pages/Sale/index.tsx
@@ -323,7 +323,7 @@ const Sale: React.FC<IProps> = () => {
   const getNfceDanfe = async (sale: SaleFromApi) => {
     const { data } = await axios({
       method: "GET",
-      url: `${window.Main.env.API_SALES_HANDLER}/nfce/${sale.ref}/danfe`,
+      url: `${window.Main.env.API_SALES_HANDLER}/nfce/${sale.id}/danfe`,
       headers: {
         Authorization: `Bearer ${user.token}`,
       },
