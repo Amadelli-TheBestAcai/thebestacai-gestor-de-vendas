209f2f0560885641c2ef11221ad6371b5aeced59
Pedro Bragatto Neto <134211594+pbragatto@users.noreply.github.com>
2023-11-24 12:33:01 -0300

    style: estilizando observação de alterar loja

--- src/pages/Settings/styles.ts
+++ src/pages/Settings/styles.ts
@@ -96,6 +96,8 @@ export const ButtonSave = styled(ButtonAnt)`
   border: none;
   box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
 
+  margin-right: 1.5rem;
+
   :active,
   :hover,
   :focus {
@@ -186,6 +188,63 @@ export const Switch = styled(SwitchAnt)`
   /*Responsive 1366px*/
   @media (max-width: 1366px) {
     zoom: 0.8;
+  }
+`;
+
+export const StoreChange = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+
+  .descriptionStore {
+    margin-left: 10px;
+  }
+  .storeObservation {
+    border-left: 1px solid;
+    margin-left: 1rem;
+    padding: 2px;
+    color: var(--grey-80);
+    font-size: 14px;
+  }
+`;
+
+export const ButtonSaveStore = styled(ButtonAnt)`
+  width: 113px;
+  height: 50px;
+  background: var(--orange-250);
+  color: var(--brown-500);
+  border-radius: 5px;
+  font-weight: 500;
+  border: none;
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+
+  margin-right: 1.5rem;
+
+  &.disabled-button {
+    ${DisabledButtonStyle}
+  }
+
+  :active,
+  :hover,
+  :focus {
+    background: var(--orange-250);
+    color: var(--brown-500);
+    border: none;
+  }
+
+  /*Responsive 1600px*/
+  @media (max-width: 1600px) {
+    @media (max-height: 900px) {
+      width: 105px;
+      height: 40px;
+    }
+  }
+
+  /*Responsive 1366px*/
+  @media (max-width: 1366px) {
+    width: 100px;
+    height: 35px;
+    font-size: 0.8rem;
   }
 `;
 
