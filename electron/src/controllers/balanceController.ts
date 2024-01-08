a0163a617a7fa7e2e15cfd9cd71f68e8a39a37e0
Wirlley Delfino <wirlley_d@hotmail.com>
2024-01-08 17:53:33 -0300

    refatorando conexao com balanca

--- electron/src/controllers/balanceController.ts
+++ electron/src/controllers/balanceController.ts
@@ -1,9 +1,6 @@
-import SerialPort from "serialport";
 import { ipcMain } from "electron";
-import { sleep } from "../helpers/sleep";
 import { settingsFactory } from "../factories/settingsFactory";
-
-let port: SerialPort | null = null;
+import { digitalBalance } from "../helpers/DigitalBalance";
 
 ipcMain.on("balance:connect", async (event) => {
   try {
@@ -19,127 +16,33 @@ ipcMain.on("balance:connect", async (event) => {
       return;
     }
 
-    if (port && port.isOpen) {
+    if (digitalBalance.port && digitalBalance.port.isOpen) {
       return;
     }
 
-    port = new SerialPort(
-      settings?.balance_port,
-      {
-        baudRate: 9600,
-        dataBits: 7,
-        stopBits: 1,
-        parity: "none",
-        autoOpen: true,
-      },
-      () => {
-        port?.on("error", function (err: any) {
-          event.reply("balance:get:response", { error: true });
-        });
-      }
-    );
+    digitalBalance.portName = settings?.balance_port;
+    await digitalBalance.connect();
   } catch (err) {
     console.error(err);
   }
 });
 
 ipcMain.on("balance:get", async (event) => {
-  port?.once("data", async function (data: any) {
-    if (!data.toString().match(/\d+/)) {
-      return event.reply("balance:get:response", { weight: 0 });
-    }
-    const result = data.toString().match(/\d+/)[0];
-    const resultFormatedToNumber = +result.replace(
-      /(\d)(?=(\d{3})+(?!\d))/g,
-      "$1."
-    );
-    return event.reply("balance:get:response", {
-      weight: resultFormatedToNumber,
-    });
-  });
-
-  port?.write("SYST:ADDR?\n", function (err: any) {
-    err && console.log("err: " + err);
-  });
-  await sleep(2000);
-  return event.reply("balance:get:response", { weight: 0 });
+  const weight = await digitalBalance.read();
+  return event.reply("balance:get:response", { weight });
 });
 
 ipcMain.on("balance:testConnection", async (event, portCOM) => {
-  try {
-
-    if (port && port.isOpen) {
-      port?.close(function (err) {
-        if (err) {
-          console.log("Error closing port: ", err.message);
-          return event.reply("balance:testConnection:response", {
-            success: false,
-            message: `Erro ao tentar desconectar a porta ${port?.path}`,
-          });
-        }
-
-      });
-      return event.reply("balance:testConnection:response", {
-        success: false,
-        message: `A porta ${port?.path} foi desconectada`,
-      });
-    }
-
-    port = new SerialPort(
-      portCOM,
-      {
-        baudRate: 9600,
-        dataBits: 7,
-        stopBits: 1,
-        parity: "none",
-        autoOpen: true,
-      },
-      (err) => {
-        if (err) {
-          const errorFileNotFound = err?.message.toLowerCase().includes("file not found");
-          const errorAccessDenied = err?.message.toLowerCase().includes("access denied");
-          if (errorFileNotFound) {
-            return event.reply("balance:testConnection:response", {
-              success: false,
-              message: `A porta ${port?.path} não foi encontrada`,
-            });
-          }
-          if (errorAccessDenied) {
-            return event.reply("balance:testConnection:response", {
-              success: false,
-              message:
-                `A porta ${port?.path} já está sendo utilizada por outro dispositivo`,
-            });
-          }
-          console.log(err);
-          return event.reply("balance:testConnection:response", {
-            success: false,
-            message: "Falha ao conectar a balança",
-            error: err,
-          });
-        } else {
-          port?.on("error", function (err: any) {
-            console.log(err);
-            return event.reply("balance:testConnection:response", {
-              success: false,
-              message: "Falha ao conectar a balança",
-              error: err,
-            });
-          });
-          return event.reply("balance:testConnection:response", {
-            success: true,
-            message: "Conexão com a balança foi estabelecida com sucesso",
-            error: err,
-          });
-        }
-      }
-    );
-  } catch (error) {
-    console.log(error);
+  if (digitalBalance.port && digitalBalance.port.isOpen) {
+    digitalBalance.disconnect();
     return event.reply("balance:testConnection:response", {
       success: false,
-      message: "Falha ao conectar a balança",
-      error,
+      message: `A porta ${digitalBalance.port?.path} foi desconectada`,
     });
   }
+
+  digitalBalance.portName = portCOM;
+  const response = await digitalBalance.connect();
+  console.log({ response });
+  return event.reply("balance:testConnection:response", response);
 });
