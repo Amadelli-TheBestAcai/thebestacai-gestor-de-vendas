928e49d12d076c45f0c25847438dc126a5d146a0
Wirlley Delfino <wirlley_d@hotmail.com>
2024-04-08 11:00:54 -0300

    Habilitando clube ao informar cpf e correção de integração de movimentação

--- electron/src/usecases/storeCash/openOnlineStoreCash.ts
+++ electron/src/usecases/storeCash/openOnlineStoreCash.ts
@@ -1,5 +1,5 @@
 import { BaseRepository } from "../../repository/baseRepository";
-import database from '../../providers/database';
+import database from "../../providers/database";
 import { IUseCaseFactory } from "../useCaseFactory.interface";
 import { StorageNames } from "../../repository/storageNames";
 import odinApi from "../../providers/odinApi";
@@ -10,11 +10,12 @@ class OpenOnlineStoreCash implements IUseCaseFactory {
   constructor(
     private storeCashRepository = new BaseRepository<StoreCashDto>(
       StorageNames.StoreCash
-
     ),
     private storeRepository = new BaseRepository<StoreDto>(StorageNames.Store),
-    private settingsRepository = new BaseRepository<SettingsDto>(StorageNames.Settings)
-  ) { }
+    private settingsRepository = new BaseRepository<SettingsDto>(
+      StorageNames.Settings
+    )
+  ) {}
 
   async execute(): Promise<StoreCashDto | undefined> {
     const hasConnection = await checkInternet();
@@ -22,45 +23,43 @@ class OpenOnlineStoreCash implements IUseCaseFactory {
       throw new Error("O sistema está offline");
     }
 
-    const storeCash = await this.storeCashRepository.getOne() as StoreCashDto;
-    const store = await this.storeRepository.getOne() as StoreDto;
-    const settings = await this.settingsRepository.getOne() as SettingsDto;
+    const storeCash = (await this.storeCashRepository.getOne()) as StoreCashDto;
+    const store = (await this.storeRepository.getOne()) as StoreDto;
+    const settings = (await this.settingsRepository.getOne()) as SettingsDto;
 
     if (settings.should_open_casher === false) {
-      throw new Error("Nenhum caixa está disponível para abertura, entre em contato com o suporte");
+      throw new Error(
+        "Nenhum caixa está disponível para abertura, entre em contato com o suporte"
+      );
     }
 
     const {
       data: { data },
-    } = await odinApi.get(
-      `/store_cashes/${store?.company_id}/summary`
-    );
+    } = await odinApi.get(`/store_cashes/${store?.company_id}/summary`);
 
     const { closed } = data;
 
     const cashes: string[] = closed.map((cash) => cash.split("-")[1]);
 
     if (!cashes.length) {
-      throw new Error("Nenhum caixa está disponível para abertura, entre em contato com o suporte");
+      throw new Error(
+        "Nenhum caixa está disponível para abertura, entre em contato com o suporte"
+      );
     }
 
     const code = cashes[0];
 
     const {
       data: {
-        data: { cash_id, history_id, store_id },
+        data: { cash_id, history_id },
       },
-    } = await odinApi.put(
-      `/store_cashes/${store?.company_id}-${code}/open`,
-      {
-        amount_on_open: storeCash.amount_on_open.toString() || "0",
-      }
-    );
+    } = await odinApi.put(`/store_cashes/${store?.company_id}-${code}/open`, {
+      amount_on_open: storeCash.amount_on_open.toString() || "0",
+    });
 
     storeCash.code = code;
     storeCash.cash_id = cash_id;
     storeCash.history_id = history_id;
-    storeCash.store_id = store_id;
     storeCash.is_online = true;
 
     await this.storeCashRepository.update(storeCash.id, storeCash);
