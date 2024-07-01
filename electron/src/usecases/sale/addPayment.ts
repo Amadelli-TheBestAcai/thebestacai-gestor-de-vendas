dc83e2c7b72bdf370be858e5057c53b945d53143
Lucas Silva <lucas.augusto7969@gmail.com>
2024-07-01 11:43:44 -0300

    feat: add propriedade que desliga o pix com tef

--- electron/src/usecases/sale/addPayment.ts
+++ electron/src/usecases/sale/addPayment.ts
@@ -15,6 +15,7 @@ interface Request {
   amount: number;
   type: number;
   flag_card?: number;
+  turnOnTefPix?: boolean
 }
 
 class AddPayment implements IUseCaseFactory {
@@ -26,7 +27,7 @@ class AddPayment implements IUseCaseFactory {
 
   ) { }
 
-  async execute({ amount, type, flag_card }: Request): Promise<SaleDto> {
+  async execute({ amount, type, flag_card, turnOnTefPix }: Request): Promise<SaleDto> {
     const isConnectInternet = await checkInternet();
 
     const { response: sale, has_internal_error: errorOnGetCurrentSale } =
@@ -43,7 +44,7 @@ class AddPayment implements IUseCaseFactory {
     let numero_autorizacao;
     let tef_status_payment;
 
-    if (settings?.should_use_tef && type !== PaymentType.DINHEIRO && isConnectInternet) {
+    if (settings?.should_use_tef && type !== PaymentType.DINHEIRO && isConnectInternet && turnOnTefPix) {
       const { response, has_internal_error: errorOnGetCurrentSale, error_message } =
         await useCaseFactory.execute<any>(this.transactionsTefUseCase, { type, amount });
 
