de7aaa20fd0ab6dba8dd75fec58679877c5ee937
Lucas Silva <lucas.augusto7969@gmail.com>
2024-07-03 11:21:44 -0300

    feat: add verificação se tem certeza que deseja fechr o gestor

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, screen, ipcMain } from "electron";
+import { app, BrowserWindow, screen, ipcMain, dialog } from "electron";
 import { autoUpdater } from "electron-updater";
 import * as path from "path";
 import { inicializeControllers } from "./src/controllers";
@@ -32,6 +32,18 @@ function createWindow() {
 
   win.on("page-title-updated", function (e) {
     e.preventDefault();
+  });
+
+  win.on("close", (e) => {
+    const choice = dialog.showMessageBoxSync(win as any, {
+      type: "question",
+      buttons: ["Sim", "Não"],
+      title: "Confirmar",
+      message: "Tem certeza que deseja fechar o Gestor de Vendas?",
+    });
+    if (choice === 1) {
+      e.preventDefault();
+    }
   });
 
   if (app.isPackaged) {
