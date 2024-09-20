9c7d17f0ed88ea65a805d03a5457f04a751ef2fe
Luis <luis.stelmastchuk@gmail.com>
2024-09-20 10:24:59 -0300

    fix: ajuste função printDanfe

--- electron/src/usecases/common/printDanfe.ts
+++ electron/src/usecases/common/printDanfe.ts
@@ -15,40 +15,87 @@ function extractDataFromHTML(htmlContent) {
   const { window } = new JSDOM(htmlContent);
   const { document } = window;
 
-  const data = {
-    dados_da_empresa: Array.from(
-      document.querySelectorAll("#dados-da-empresa td")
-    ).map((td: any) => td.textContent.trim()),
-    documento_auxiliar: Array.from(
-      document.querySelectorAll("#documento-auxiliar td")
-    ).map((td: any) => td.textContent.trim()),
-    lista_produtos: Array.from(
-      document.querySelectorAll("#lista-produtos td")
-    ).map((td: any) => td.textContent.trim()),
-    valor_total: Array.from(document.querySelectorAll("#valor-total td")).map(
-      (td: any) => td.textContent.trim()
-    ),
-    forma_pagamento: Array.from(
-      document.querySelectorAll("#forma-pagamento td")
-    ).map((td: any) => td.textContent.trim()),
-    tributacao: Array.from(document.querySelectorAll("#tributacao td")).map(
-      (td: any) => td.textContent.trim()
-    ),
-    info_adicionais: Array.from(
-      document.querySelectorAll("#info-adicionais td")
-    ).map((td: any) => td.textContent.trim()),
-    homologacao: Array.from(document.querySelectorAll("#homologacao td")).map(
-      (td: any) => td.textContent.trim()
-    ),
-    consumidor: Array.from(document.querySelectorAll("#consumidor td")).map(
-      (td: any) => td.textContent.trim()
-    ),
-    qrcode: [
-      document.querySelector("#qrcode span").textContent.trim(),
-      htmlContent.split(`text: "`)[1].split(`"`)[0],
-      "Protocolo de autorização:  " +
-        htmlContent.split(`Protocolo de autorização: `)[1].split("<")[0],
-    ],
+  let data;
+
+  const dados_da_empresa_temp = document.querySelectorAll(
+    ".dados-da-empresa td"
+  )
+    ? Array.from(document.querySelectorAll(".dados-da-empresa td")).map(
+        (td: any) => td?.textContent?.trim()
+      )
+    : "";
+
+  const documento_auxiliar_temp = document.querySelectorAll(
+    ".documento-auxiliar td"
+  )
+    ? Array.from(document.querySelectorAll(".documento-auxiliar td")).map(
+        (td: any) => td?.textContent?.trim()
+      )
+    : "";
+
+  const lista_produtos_temp = document.querySelectorAll(".lista-produtos td")
+    ? Array.from(document.querySelectorAll(".lista-produtos td")).map(
+        (td: any) => td?.textContent?.trim()
+      )
+    : "";
+
+  const valor_total_temp = document.querySelectorAll(".valor-total td")
+    ? Array.from(document.querySelectorAll(".valor-total td")).map((td: any) =>
+        td?.textContent?.trim()
+      )
+    : "";
+
+  const forma_pagamento_temp = document.querySelectorAll(".forma-pagamento td")
+    ? Array.from(document.querySelectorAll(".forma-pagamento td")).map(
+        (td: any) => td?.textContent?.trim()
+      )
+    : "";
+
+  const tributacao_temp = document.querySelectorAll(".tributacao td")
+    ? Array.from(document.querySelectorAll(".tributacao td")).map((td: any) =>
+        td?.textContent?.trim()
+      )
+    : "";
+
+  const info_adicionais_temp = document.querySelectorAll(".info-adicionais td")
+    ? Array.from(document.querySelectorAll(".info-adicionais td")).map(
+        (td: any) => td?.textContent?.trim()
+      )
+    : "";
+
+  const homologacao_temp = document.querySelectorAll(".homologacao td")
+    ? Array.from(document.querySelectorAll(".homologacao td")).map((td: any) =>
+        td?.textContent?.trim()
+      )
+    : "";
+
+  const consumidor_temp = document.querySelectorAll(".consumidor td")
+    ? Array.from(document.querySelectorAll(".consumidor td")).map((td: any) =>
+        td?.textContent?.trim()
+      )
+    : "";
+
+  const qrcode_temp = document.querySelector(".qrcode span")
+    ? [
+        document.querySelector(".qrcode span").textContent?.trim(),
+        htmlContent?.split(`text: "`)[1]?.split(`"`)[0],
+        "Protocolo de autorização:  " +
+          htmlContent?.split(`Protocolo de autorização: `)[1]?.split("<")[0],
+      ]
+    : "";
+
+  data = {
+    ...data,
+    dados_da_empresa: dados_da_empresa_temp,
+    documento_auxiliar: documento_auxiliar_temp,
+    lista_produtos: lista_produtos_temp,
+    valor_total: valor_total_temp,
+    forma_pagamento: forma_pagamento_temp,
+    tributacao: tributacao_temp,
+    info_adicionais: info_adicionais_temp,
+    homologacao: homologacao_temp,
+    consumidor: consumidor_temp,
+    qrcode: qrcode_temp,
   };
 
   return data;
@@ -88,15 +135,15 @@ class PrintDanfe implements IUseCaseFactory {
     const printer = settings?.printer;
 
     const html = await ipcRenderer.invoke("get-danfe", payload);
-
+    
     const content = extractDataFromHTML(html);
-
+    
     const termalPrinter = Printer.getPrinter(printer);
 
     this.printerFormater.clear();
 
     this.printerFormater.alignLeft();
-    content.dados_da_empresa.forEach((info) =>
+    content?.dados_da_empresa.forEach((info) =>
       this.printerFormater.println(info)
     );
 
@@ -116,35 +163,35 @@ class PrintDanfe implements IUseCaseFactory {
     ]);
 
     let count = 0;
-    while (count <= content.lista_produtos.length - 6) {
+    while (count <= content?.lista_produtos.length - 6) {
       this.printerFormater.tableCustom([
         {
-          text: content.lista_produtos[count],
+          text: content?.lista_produtos[count],
           align: "LEFT",
           cols: 5,
         },
         {
-          text: content.lista_produtos[count + 1],
+          text: content?.lista_produtos[count + 1],
           align: "CENTER",
           cols: 10,
         },
         {
-          text: content.lista_produtos[count + 2],
+          text: content?.lista_produtos[count + 2],
           align: "CENTER",
           cols: 10,
         },
         {
-          text: content.lista_produtos[count + 3],
+          text: content?.lista_produtos[count + 3],
           align: "CENTER",
           cols: 5,
         },
         {
-          text: content.lista_produtos[count + 4],
+          text: content?.lista_produtos[count + 4],
           align: "CENTER",
           cols: 10,
         },
         {
-          text: content.lista_produtos[count + 5],
+          text: content?.lista_produtos[count + 5],
           align: "CENTER",
           cols: 10,
         },
@@ -155,39 +202,39 @@ class PrintDanfe implements IUseCaseFactory {
 
     this.printerFormater.tableCustom([
       {
-        text: content.valor_total[0],
+        text: content?.valor_total[0],
         align: "LEFT",
         cols: 25,
       },
       {
-        text: content.valor_total[1],
+        text: content?.valor_total[1],
         align: "RIGHT",
         cols: 20,
       },
     ]);
     this.printerFormater.tableCustom([
       {
-        text: content.valor_total[2],
+        text: content?.valor_total[2],
         align: "LEFT",
         cols: 25,
       },
       {
-        text: content.valor_total[3],
+        text: content?.valor_total[3],
         align: "RIGHT",
         cols: 20,
       },
     ]);
 
     count = 0;
-    while (count <= content.forma_pagamento.length - 2) {
+    while (count <= content?.forma_pagamento.length - 2) {
       this.printerFormater.tableCustom([
         {
-          text: content.forma_pagamento[count],
+          text: content?.forma_pagamento[count],
           align: "LEFT",
           cols: 25,
         },
         {
-          text: content.forma_pagamento[count + 1],
+          text: content?.forma_pagamento[count + 1],
           align: "RIGHT",
           cols: 20,
         },
@@ -199,12 +246,12 @@ class PrintDanfe implements IUseCaseFactory {
 
     this.printerFormater.tableCustom([
       {
-        text: content.tributacao[0],
+        text: content?.tributacao[0],
         align: "LEFT",
         cols: 25,
       },
       {
-        text: content.tributacao[1],
+        text: content?.tributacao[1],
         align: "RIGHT",
         cols: 20,
       },
@@ -213,22 +260,22 @@ class PrintDanfe implements IUseCaseFactory {
     this.printerFormater.drawLine();
 
     this.printerFormater.alignCenter();
-    content.homologacao.forEach((info) => this.printerFormater.println(info));
+    content?.homologacao.forEach((info) => this.printerFormater.println(info));
 
     this.printerFormater.drawLine();
 
     this.printerFormater.alignCenter();
-    content.consumidor.forEach((info) => this.printerFormater.println(info));
+    content?.consumidor.forEach((info) => this.printerFormater.println(info));
 
     this.printerFormater.drawLine();
 
     this.printerFormater.alignCenter();
-    this.printerFormater.println(content.qrcode[0]);
-    this.printerFormater.printQR(content.qrcode[1], {
+    this.printerFormater.println(content?.qrcode[0]);
+    this.printerFormater.printQR(content?.qrcode[1], {
       correction: "M",
       cellSize: 7,
     });
-    this.printerFormater.println(content.qrcode[2]);
+    this.printerFormater.println(content?.qrcode[2]);
 
     this.printerFormater.cut();
 
