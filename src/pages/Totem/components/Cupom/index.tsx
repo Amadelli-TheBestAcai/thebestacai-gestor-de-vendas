a407b889ec4a0c3dcd3c8eab19dd3e4300dea420
Luis <luis.stelmastchuk@gmail.com>
2025-03-18 15:47:16 -0300

    feature: estilização da page de copum adicionado

--- src/pages/Totem/components/Cupom/index.tsx
+++ src/pages/Totem/components/Cupom/index.tsx
@@ -1,5 +1,6 @@
 import React, { Dispatch, SetStateAction, useState } from "react";
 
+import cupom from "../../../../assets/totem/svg/cupom.svg";
 import arrow_left from "../../../../assets/totem/svg/arrow_left.svg";
 
 import { useSale } from "../../../../hooks/useSale";
@@ -21,11 +22,19 @@ interface IProps {
 }
 const Cupom: React.FC<IProps> = ({ setStep }) => {
   const { sale, setSale } = useSale();
-  const [cupom, setCupom] = useState<string>("");
-  const [stepCupomPage, setSetpCupomPage] = useState<1 | 2>(1);
+  const [cupomInput, setCupomInput] = useState<string>("");
+  const [validCupom, setValidCupom] = useState<string>("Teste");
+  const [stepCupomPage, setStepCupomPage] = useState<1 | 2>(1);
 
   const onAddCupom = async () => {
-    setSetpCupomPage(2);
+    setStepCupomPage(2);
+  };
+
+  const returnCheckOut = async () => {
+    setStepCupomPage(1);
+    setCupomInput("");
+    setValidCupom("");
+    setStep(4);
   };
 
   return (
@@ -38,14 +47,22 @@ const Cupom: React.FC<IProps> = ({ setStep }) => {
           <Body style={{ padding: "6rem 0 0" }}>
             <span className="span-cupom">INFORME O CÓDIGO DO CUPOM</span>
             <div className="inputContainer">
-              <Input value={cupom} placeholder={"Código do cupom"} disabled />
+              <Input
+                value={cupomInput}
+                placeholder={"Código do cupom"}
+                disabled
+              />
             </div>
             <div className="div-pinpad">
-              <VirtualPinpad value={cupom} setValue={setCupom} maxLenght={6} />
+              <VirtualPinpad
+                value={cupomInput}
+                setValue={setCupomInput}
+                maxLenght={8}
+              />
             </div>
           </Body>
           <Footer>
-            <Button onClick={() => setStep(4)}>
+            <Button onClick={() => returnCheckOut()}>
               <img src={arrow_left} />
               Voltar
             </Button>
@@ -64,10 +81,30 @@ const Cupom: React.FC<IProps> = ({ setStep }) => {
           <Header>
             <span>Cupom inserido com sucesso!</span>
           </Header>
+          <Body style={{ padding: "6rem 0 0" }}>
+            <span className="span-cupom-title">
+              <img src={cupom} />
+              {validCupom || ""}
+            </span>
+            <img src={cupom} className="cupom-img" />
+            <div className="cupom-description">
+              Válido apenas na Black Friday, 29/11.
+              <br /> Promoção válida em todas as lojas da The Best Açaí. Cupons
+              ilimitados, resgate quantos quiser.
+              <br />
+              Cupons são específicos de cada loja.
+              <br /> Para cada pagamento no caixa, é necessário gerar um novo
+              cupom. <br />
+              Não é válido para potes que contenham apenas acompanhamentos,
+              Promoção não cumulativa com outras promoções.
+              <br /> A utilização desse cupom inviabiliza o acúmulo de pontos no
+              Clube The Best.
+            </div>
+          </Body>
           <Footer>
             <ButtonFinalize
               onClick={() => {
-                onAddCupom();
+                returnCheckOut();
               }}
               step={stepCupomPage}
             >
