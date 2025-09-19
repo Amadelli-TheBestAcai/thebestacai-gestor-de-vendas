de4e66ea3fcbdb96d8b01fb7d92ad7b6cdc326a9
Lucas Silva <lucas.augusto7969@gmail.com>
2025-09-19 11:57:14 -0300

    feat: adicionar lógica para sincronizar vendas não integradas e resgatar recompensas

--- electron/src/usecases/sale/synchronizeSales.ts
+++ electron/src/usecases/sale/synchronizeSales.ts
@@ -1,9 +1,13 @@
 import { IUseCaseFactory } from "../useCaseFactory.interface";
-import { SaleDto, StoreCashDto } from "../../models/gestor";
+import { SaleDto, StoreCashDto, StoreDto } from "../../models/gestor";
 import { BaseRepository } from "../../repository/baseRepository";
 import { StorageNames } from "../../repository/storageNames";
 import { useCaseFactory } from "../useCaseFactory";
 import { getSaleFromApi } from "./getSaleFromApi";
+import midasApi from "../../providers/midasApi";
+import { salesFormaterToIntegrate } from "../../helpers/salesFormaterToIntegrate";
+import { redeemReward } from "./redeemReward";
+import { getUser } from "../user";
 
 class SynchronizeSales implements IUseCaseFactory {
     constructor(
@@ -16,10 +20,19 @@ class SynchronizeSales implements IUseCaseFactory {
         private notIntegratedSaleRepository = new BaseRepository<SaleDto>(
             StorageNames.Not_Integrated_Sale
         ),
-        private getSaleFromApiUseCase = getSaleFromApi
+        private storeRepository = new BaseRepository<StoreDto>(StorageNames.Store),
+        private getSaleFromApiUseCase = getSaleFromApi,
+        private redeemRewardUseCase = redeemReward,
+        private getUserUseCase = getUser
     ) { }
 
     async execute(): Promise<void> {
+        const store = (await this.storeRepository.getOne()) as StoreDto;
+
+        if (!store) {
+            throw new Error("Não foi possível obter os dados da loja.");
+        }
+
         const storeCash = await this.storeCashRepository.getOne();
 
         if (!storeCash) {
@@ -52,6 +65,40 @@ class SynchronizeSales implements IUseCaseFactory {
         );
 
         console.log(salesNotInApi, 'salesNotInApi')
+
+        try {
+            if (salesNotInApi.length) {
+                await Promise.all(
+                    salesNotInApi.map(async (salePayload) => {
+                        try {
+                            if (salePayload.customer_reward_id) {
+                                const user = await this.getUserUseCase.execute();
+                                const redeemPayload = {
+                                    store_id: store.company_id,
+                                    user_name: user?.name as string,
+                                    user_id: user?.id as number,
+                                    company_name: store.company.company_name,
+                                };
+                                await this.redeemRewardUseCase.execute({
+                                    id: salePayload.customer_reward_id,
+                                    payload: redeemPayload,
+                                });
+                            }
+                            let payload = salesFormaterToIntegrate(salePayload, storeCash);
+                            payload = payload.map((sale) => ({
+                                ...sale,
+                                store_id: storeCash.store_id,
+                            }));
+                            await midasApi.post("/sales", payload);
+                        } catch (error) {
+                            console.log(error);
+                        }
+                    })
+                );
+            }
+        } catch (error) {
+            console.log(error);
+        }
 
     }
 }
