a3f17ea4428c5ca99c81056d960aecf1cc4cf48e
Lucas Silva <lucas.augusto7969@gmail.com>
2025-10-01 11:40:49 -0300

    feat: add get de pdf

---
+++ electron/src/usecases/filesManagement/getPdfTheBestGamesFiles.ts
@@ -0,0 +1,25 @@
+import { checkInternet } from "../../providers/internetConnection";
+import { IUseCaseFactory } from "../useCaseFactory.interface";
+import janusApi from "../../providers/janusApi";
+
+class GetPdfTheBestGamesFiles implements IUseCaseFactory {
+  async execute(): Promise<Buffer> {
+    const isConnectInternet = await checkInternet();
+    if (!isConnectInternet) {
+      throw new Error(
+        "Sem conexão com a internet. Verifique sua conexão para usar o serviço TEF e tente novamente."
+      );
+    }
+
+    const { data } = await janusApi.get(
+      `/files-management/ti/files-the-best-games/teste-the-best-game.pdf/beautify`,
+      {
+        responseType: "arraybuffer",
+      }
+    );
+
+    return Buffer.from(data, "binary");
+  }
+}
+
+export const getPdfTheBestGamesFiles = new GetPdfTheBestGamesFiles();
