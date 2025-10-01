01d4c37e807173c7fac0d9be6238febcf0d4ec16
Lucas Silva <lucas.augusto7969@gmail.com>
2025-10-01 11:42:16 -0300

    feat: add rota de pdf

--- electron/src/factories/filesManagementFactory.ts
+++ electron/src/factories/filesManagementFactory.ts
@@ -1,8 +1,10 @@
-import { getImageTheBestGamesFiles } from "../usecases/filesManagement";
+import { getImageTheBestGamesFiles, getPdfTheBestGamesFiles } from "../usecases/filesManagement";
 import { useCaseFactory } from "../usecases/useCaseFactory";
 
 
 export const filesManagementFactory = {
     getImageTheBestGamesFiles: async () =>
         await useCaseFactory.execute<Buffer>(getImageTheBestGamesFiles),
+    getPdfTheBestGamesFiles: async () =>
+        await useCaseFactory.execute<Buffer>(getPdfTheBestGamesFiles),
 };
--- electron/src/usecases/filesManagement/index.ts
+++ electron/src/usecases/filesManagement/index.ts
@@ -1,1 +1,2 @@
 export * from './getImageTheBestGamesFiles'
+export * from './getPdfTheBestGamesFiles'
