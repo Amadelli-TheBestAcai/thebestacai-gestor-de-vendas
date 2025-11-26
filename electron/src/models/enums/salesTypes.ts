36d5b743e845cd04064ab74b8415fde15f8bd951
Lucas Silva <lucas.augusto7969@gmail.com>
2025-11-26 10:28:45 -0300

    feat: add novo type

--- electron/src/models/enums/salesTypes.ts
+++ electron/src/models/enums/salesTypes.ts
@@ -1,10 +1,11 @@
 export enum SalesTypes {
-  LOJA = 0,
+  STORE = 0,
   IFOOD = 1,
   UBBEREATS = 2,
   WHATSAPP = 3,
   TELEFONE = 4,
   APP = 5,
-  OUTROS = 6,
+  OTHERS = 6,
   TOTEM = 7,
+  PEDIDOS_YA = 8
 }
